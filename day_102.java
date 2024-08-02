3ca2b607c0cddcee3d338b5983a93033b52e8455
Krishna K <krish1826s@gmail.com>
2024-08-02 21:35:38 +0530

    Create day_102.java

---
+++ day_102.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+         Scanner sc=new Scanner(System.in);
+         int n=sc.nextInt();
+         while(n-->0){
+             System.out.println((sc.nextInt())*15);
+         }
+	}
+}

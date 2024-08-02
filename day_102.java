938c8db1acea98edc048ece74a9ac2a083fd15ab
Krishna K <krish1826s@gmail.com>
2024-08-02 23:19:37 +0530

    Update day_102.java

--- day_102.java
+++ day_102.java
@@ -29,3 +29,32 @@ public class Hello {
 
 	}
 }
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Same FreQuency pattern
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.next();
+		String s2=sc.next();
+		int[] f1=frequency(s1);
+		int[] f2=frequency(s2);
+		System.out.print(isSameFrequency(f1,f2));
+
+	}
+	static String isSameFrequency(int[] fr1,int[] fr2){
+	    Arrays.sort(fr2);
+	    Arrays.sort(fr1);
+	    for(int i=0;i<256;i++){
+	        if(fr1[i]!=fr2[i]) return "no";
+	    }
+	    return "yes";
+	}
+	static int[] frequency(String ip){
+	    int[] freq=new int[256];
+	    for(char i:ip.toCharArray()){
+	        freq[(int)i]++;
+	    }
+	    return freq;
+	}
+}

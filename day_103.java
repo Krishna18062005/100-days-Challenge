c22eaa87b55e8aa285fa904ac483c0cf15c54f88
Krishna K <krish1826s@gmail.com>
2024-08-04 00:11:18 +0530

    Update day_103.java

--- day_103.java
+++ day_103.java
@@ -36,3 +36,68 @@ public class Hello {
 	    return ans;
 	}
 }
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Matric - Integers / Alphabets 
+		Scanner sc=new Scanner(System.in);
+		int r=sc.nextInt();
+		int c=sc.nextInt();
+		char[][] mat=new char[r][c];
+		for(int i=0;i<r;i++) for(int j=0;j<c;j++) mat[i][j]=sc.next().charAt(0);
+		int numc=0;
+		int alpc=0;
+		for(char[] i:mat){
+		    for(char j:i){
+		        if(((int)j-'0'<=9) &&((int)j-'0'>=0)) numc++;
+		        else alpc++;
+		    }
+		}
+		int sq=(int)Math.sqrt(numc);
+		int sq2=(int)Math.sqrt(alpc);
+		if(numc==sq*sq&&alpc==sq2*sq2) {
+		    if(alpc>=numc){
+		        print(0,sq2,mat);
+		    }
+		    else{
+		        print(1,sq,mat);
+		    }
+		}
+		else if(numc==sq*sq) print(1,sq,mat);
+		else print(0,sq2,mat);
+		
+	}
+	static void print(int a,int count,char[][] ma){
+	    if(a==1){
+	        int c=0;
+	        for(char[] i:ma){
+	            for(char j:i){
+	                if((int)j-'0'<=9&&(int)j-'0'>=0){
+	                    c++;
+	                    System.out.print(j+" ");
+	                }
+	                if(c==count){
+	                    System.out.println();
+	                    c=0;
+	                }
+	            }
+	        }
+	    }
+	    else{
+	        int c=0;
+	        for(char[] i:ma){
+	            for(char j:i){
+	                if(!((int)j-'0'<=9&&(int)j-'0'>=0)) {
+	                    System.out.print(j+" ");
+	                    c++;
+	                }
+	                if(c==count){
+	                    System.out.println();
+	                    c=0;
+	                }
+	            }
+	        }
+	    }
+	}
+}

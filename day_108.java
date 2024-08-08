4ab2b58f5c1a943cf61d2efaff4241de982e09b6
Krishna K <krish1826s@gmail.com>
2024-08-08 21:41:18 +0530

    Update day_108.java

--- day_108.java
+++ day_108.java
@@ -1,3 +1,20 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static int ninjaAndTriangle(int n) {
+        // Write your code here.
+        int s=0;
+        int c=0;
+        for(int i=1;s+i<=n;i++){
+            s+=i;
+            c++;
+            if(s==n){
+                return i;
+            }
+        }
+        return c;
+    }
+}
 
 
 import java.util.*;

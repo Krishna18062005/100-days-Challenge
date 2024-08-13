124deebfdf0fc13fcc37de7289681aee04485a97
Krishna K <krish1826s@gmail.com>
2024-08-13 21:45:36 +0530

    Update day_113.java

--- day_113.java
+++ day_113.java
@@ -21,3 +21,29 @@ public class Hello {
 
 	}
 }
+import java.util.* ;
+import java.io.*; 
+/*******************************************
+ *
+ *   Following is the Pair class structure
+ *
+ *   class Pair{
+ *
+ *       char firstLetterOfName;
+ *       int avgMarks;
+ *
+ *       Pair(char firstLetterOfName, int avgMarks){
+ *           this.firstLetterOfName = firstLetterOfName;
+ *           this.avgMarks = avgMarks;
+ *       }
+ *   } 
+ *
+ **********************************************/
+
+
+public class Solution {
+	public static Pair averageMarks(char firstLetterOfName, int m1, int m2, int m3) {
+		// Write your code here.
+		return new Pair(firstLetterOfName,(m1+m2+m3)/3);
+	}
+}

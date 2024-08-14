94be06b627f53c11f2109878ab4d8425bc3a4094
Krishna K <krish1826s@gmail.com>
2024-08-14 20:49:24 +0530

    Update day_114.java

--- day_114.java
+++ day_114.java
@@ -15,3 +15,23 @@ class Codechef
          }
 	}
 }
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Two Inverted triangles Pattern
+		int a=new Scanner(System.in).nextInt();
+		for(int i=0;i<a+1;i++){
+		    for(int j=0;j<=a*2;j++){
+		        if(i==j||i==0||j==a||i==(a*2)-j){
+		            System.out.print("*");
+		        }
+		        else
+		        System.out.print("0");
+		        
+		    }
+		    System.out.println();
+		}
+
+	}
+}

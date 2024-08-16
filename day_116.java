60b143a6251627d360e4481af21297b53d95fca4
Krishna K <krish1826s@gmail.com>
2024-08-16 21:12:29 +0530

    Update day_116.java

--- day_116.java
+++ day_116.java
@@ -19,3 +19,19 @@ class Codechef
 
 	}
 }
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Match Name & Marks
+		Scanner sc=new Scanner(System.in);
+		String[] name=sc.nextLine().split(" ");
+		String[] Marks=sc.nextLine().split(" ");
+		for(String i:name){
+		    for(String j:Marks){
+		        if(i.charAt(0)==j.charAt(0)&&i.charAt(i.length()-1)==j.charAt(j.length()-1)) System.out.println(i+" "+j.substring(1,j.length()-1));
+		    }
+		}
+
+	}
+}

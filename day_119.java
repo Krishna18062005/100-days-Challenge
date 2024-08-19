6e18f125e72b7462c956f8ec1e66ee5c7f0de1bd
Krishna K <krish1826s@gmail.com>
2024-08-19 20:05:02 +0530

    Update day_119.java

--- day_119.java
+++ day_119.java
@@ -24,3 +24,23 @@ public class Solution {
 	}
 
 }
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++){
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    boolean c=(a<=b&&b%a==0);
+		    System.out.println(c?"YES":"NO");
+		}
+
+	}
+}

37daffac4913905b92b9babf8b849542410ba48f
Krishna K <krish1826s@gmail.com>
2024-08-22 22:29:38 +0530

    Update day_122.java

--- day_122.java
+++ day_122.java
@@ -1,1 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++){
+		    System.out.println(Math.max(sc.nextInt(),sc.nextInt())+Math.max(sc.nextInt(),sc.nextInt()));
+		}
+
+	}
+}
+
+
 // Alternative digit sum

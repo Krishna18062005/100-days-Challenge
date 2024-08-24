a2eff9fb153908cf1cc23ca60451bdca1343765b
Krishna K <krish1826s@gmail.com>
2024-08-24 23:44:30 +0530

    Update day_124.java

--- day_124.java
+++ day_124.java
@@ -1,1 +1,29 @@
 //JUMP MAXI COUNT
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Jump On Stones;
+		int c=0;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] ar=new int[n];
+		for(int i=0;i<n;i++){
+		    ar[i]=sc.nextInt();
+		}
+		int jc=1;
+		int i=0;
+		while(i<n-1){
+		    c+=1;
+		    if(ar[i]%2!=0){
+		        jc+=2;
+		    }
+		    if(ar[i]%2==0) jc+=3;
+		    if(ar[i]%9==0) jc-=5;
+		    if(jc<1) jc=1;
+		    i+=jc;
+		}
+		System.out.println(c+"");
+
+	}
+}

a3b185fbc4c6b6aadea50016edd8d329e69c333d
Krishna K <krish1826s@gmail.com>
2024-08-28 23:43:15 +0530

    Update day_128.java

--- day_128.java
+++ day_128.java
@@ -29,3 +29,42 @@ public class Hello {
 
 
 //day 128 starts with ml concepts
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Shift Integers;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int[] ma=new int[n];
+		
+		for(int i=0;i<n;i++){
+		    ma[i]=sc.nextInt();
+		    
+		}
+		
+		int c=0;
+		while(c<k){
+		    ma=rotate(ma);
+		    c++;
+		
+		System.out.print(Arrays.toString(ma));
+}
+	}
+	static int[] rotate(int[] ma){
+	    int p=0;
+	    for(int i=0;i<ma.length;i++){
+	        if(ma[i]%2==0) {p=i;break;}
+	    }
+	    int k=0;
+	    int[] ar=new int[ma.length];
+	    for(int i=p+1;i<ma.length;i++){
+	        ar[k++]=ma[i];
+	    }
+	    for(int i=0;i<=p;i++){
+	        ar[k++]=ma[i];
+	    }
+	    return ar;
+	} 
+}

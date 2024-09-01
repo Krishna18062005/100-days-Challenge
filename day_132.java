b36d9bc68130b82352d13dae1b77f95f8add6310
Krishna K <krish1826s@gmail.com>
2024-09-01 17:56:05 +0530

    Update day_132.java

--- day_132.java
+++ day_132.java
@@ -11,3 +11,24 @@ class Solution {
         return mat;
     }
 }
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Two Digital clocks
+		Scanner sc=new Scanner(System.in);
+		String[] t1=sc.next().split(":");
+		String[] t2=sc.next().split(":");
+		String[] c1=sc.next().split(":");
+		int diff=Math.abs(Integer.parseInt(t1[0])*60+Integer.parseInt(t1[1])-(Integer.parseInt(t2[0])*60+Integer.parseInt(t2[1])));
+		
+		int clt=Integer.parseInt(c1[0])*60+Integer.parseInt(c1[1]);
+		clt+=diff;
+		System.out.print((clt/60%24)>9?clt/60%24+":":"0"+clt/60%24+":");
+		if(clt%60<=9){
+		    System.out.print(0);
+		}
+		System.out.print((clt%60));
+
+	}
+}

d2fa54e1e431e6f8d900266435535356eb3530f9
Krishna K <krish1826s@gmail.com>
2024-09-05 22:12:10 +0530

    Update day_136.java

--- day_136.java
+++ day_136.java
@@ -12,3 +12,25 @@ public class Solution {
         return ans;
     }
 }
+class Solution {
+    public int[] missingRolls(int[] rolls, int mean, int n) {
+        int sum=0;
+        for(int i:rolls) sum+=i;
+        int reqs=mean*(n+rolls.length);
+        reqs-=sum;
+        int mre=reqs/n;
+        if(sum+(n*6)<reqs+sum||mre==0||reqs<0){
+            return new int[0];
+        }
+        int ex=reqs%n;
+        int[] ans=new int[n];
+        for(int i=0;i<n;i++){
+            ans[i]=mre;
+            while(ans[i]<6&&ex>0){
+                ans[i]+=1;
+                ex--;
+            }
+        }
+        return ans;
+    }
+}

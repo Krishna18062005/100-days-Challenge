3be8d7aa9c54e4729ae7cc45a0f55d7e08a62a21
Krishna K <krish1826s@gmail.com>
2024-09-09 23:39:09 +0530

    Update day_140.java

--- day_140.java
+++ day_140.java
@@ -64,3 +64,39 @@ public class Hello {
 // 1 2 3 4 4 4 
 // 1 2 3 4 6 6 
 // 1 2 3 4 6 12 
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Integer Group Pattern 
+		int n=new Scanner(System.in).nextInt();
+		for(int i=1;i<=n;i++){
+		    int re=n-i+1;
+		    int c=0;
+		    for(int j=1;j<=n;j++){
+		        if(c==0) System.out.print("(");
+		        System.out.print(j);
+		        c++;
+		        if(c==re||j==n){
+		            c=0;
+		            System.out.print(")");
+		        }
+		        else{
+		            System.out.print(" ");
+		        }
+		    }
+		    System.out.println();
+		}
+		
+    }
+}
+
+// 5
+
+// Output:
+
+// (1 2 3 4 5)
+// (1 2 3 4)(5)
+// (1 2 3)(4 5)
+// (1 2)(3 4)(5)
+// (1)(2)(3)(4)(5)

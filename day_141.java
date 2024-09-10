fcc86bec350be16b49ff50c9b57ea4737e476bc6
Krishna K <krish1826s@gmail.com>
2024-09-10 21:26:04 +0530

    Update day_141.java

--- day_141.java
+++ day_141.java
@@ -61,3 +61,35 @@ public class Hello {
 //uprl
 //output
 //YES
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        ListNode tem=head;
+        ListNode tem2=head.next;
+        while(tem.next!=null){
+            ListNode gcd=new ListNode(gC(tem.val,tem.next.val));
+            tem.next=gcd;
+            gcd.next=tem2;
+            tem=tem2;
+            tem2=tem2.next;
+
+        }
+        return head;
+    }
+    public static int gC(int a,int b){
+        if(b==0){
+            return a;
+        }
+        return gC(b,a%b);
+    } 
+}

5174ef233ea706a0336b656b62bdadbea5440256
Krishna K <krish1826s@gmail.com>
2024-09-15 15:16:38 +0530

    Update day_146.java

--- day_146.java
+++ day_146.java
@@ -15,3 +15,21 @@ class Solution {
         return ans;
     }
 }
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Pattern to Pattern 
+		String pattern[] =new Scanner(System.in).next().split(":");
+		for(String pat:pattern){
+		    String nums[] =pat.split("-");
+		    for(int i=0;i+1<nums.length;i++){
+		        for(int j=Integer.parseInt(nums[i]);j<=Integer.parseInt(nums[i+1]);j++){
+		            System.out.print(j+" ");
+		        }
+		        System.out.println();
+		    }
+		    
+		}
+    }
+}

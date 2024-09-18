e486238c5a90a779379489e97d5cc680da2f96ee
Krishna K <krish1826s@gmail.com>
2024-09-18 21:20:07 +0530

    Create day_149.java

---
+++ day_149.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		for(int i=0;i<a;i++){
+		    int b=sc.nextInt();
+		    int c=sc.nextInt();
+		    double d=sc.nextDouble();
+		    System.out.println(((double)(b+c)/2)>d?"YES":"NO");
+		}
+
+	}
+}

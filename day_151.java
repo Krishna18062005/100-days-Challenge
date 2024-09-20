cec09e886b5951e7d50825edf22b55220de49ee2
Krishna K <krish1826s@gmail.com>
2024-09-20 11:51:04 +0530

    Update day_151.java

--- day_151.java
+++ day_151.java
@@ -1,4 +1,32 @@
-String a="krish";
-for(int 0;i<a.length();i++){
-  System.out.print((int)a.charAt(i)+" ");
+// String a="krish";
+// for(int 0;i<a.length();i++){
+  // //bubble sort
+// // for(i=0;i<n-1;i++)
+// //   for(j=0;j<n-i-1;j++){
+// // swap if less than neighbour element //
+
+
+// // selection sort  ;;
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int[] arr={12,22,13,40,50};
+//       for(int i=0;i<arr.length-1;i++){
+//         int min=arr[i];
+//         int ind=i;
+//         for(int j=i+1;j<arr.length;j++){
+//           if(min>arr[j]){
+//             min=arr[j];
+//             ind=j;
+//           }
+//         }
+//         int t=arr[i];
+//         arr[i]=arr[ind];
+//         arr[ind]=t;
+//         System.out.println(Arrays.toString(arr));
+//       }
+//   }
+// }System.out.print((int)a.charAt(i)+" ");
 }
+// 

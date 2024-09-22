728299f8a823d59fcf8db692d410f30b34c627d5
Krishna K <krish1826s@gmail.com>
2024-09-22 08:13:52 +0530

    Update day_152.java

--- day_152.java
+++ day_152.java
@@ -1,1 +1,17 @@
 
+class Solution {
+    public boolean reportSpam(String[] message, String[] bannedWords) {
+        int c=0;
+        Set<String> se=new HashSet<>(Arrays.asList(bannedWords));
+        System.out.print(se);
+            for(String i:message){
+                if(se.contains(i)){
+                    c++;}
+                if(c>1){
+                    return true;
+                }
+        }
+        
+        return c>1;
+    }
+}

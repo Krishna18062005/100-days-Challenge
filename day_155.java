870537f98231a6963658fc0307652955bceab88a
Krishna K <krish1826s@gmail.com>
2024-10-08 20:59:02 +0530

    Update day_155.java

--- day_155.java
+++ day_155.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class day_155{
   public static void main(String[] args){
+    ///suffix of binary 
     String n=Integer.toBinaryString(new Scanner(System.in).nextInt());
     for(int i=n.length()-1;i>=0;i--){
       System.out.print(n.substring(i)+" ");

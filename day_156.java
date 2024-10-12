80b581c727664899448d053b260a30001dd5e8c2
Krishna K <krish1826s@gmail.com>
2024-10-12 20:24:47 +0530

    Update day_156.java

--- day_156.java
+++ day_156.java
@@ -1,1 +1,23 @@
 //ml federated learning basics today
+// Node is defined as:
+// class Node{
+//     int val;
+//     Node next;
+//     Node(int x){
+//     	val = x; next = null;
+//     }
+// }
+class Solution{
+    static int solve(Node root){
+    int c=0;
+    int prev=root.val;
+    root=root.next;
+    while(root.next!=null){
+        if(prev<root.val&&root.val>root.next.val) c++;
+        else if(prev>root.val&&root.val<root.next.val) c++;
+        prev=root.val;
+        root=root.next;
+    }
+    return c;
+    }
+}

43a803d8908882d9ec1e18001b616dc8c2eed07b
Krishna K <krish1826s@gmail.com>
2024-10-17 21:38:31 +0530

    Create day_160.java

---
+++ day_160.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+  
+final int K=18,U=18;
+while(K<U){
+System.out.print("CodeUniverse");
+}
+System.out.print("Critical Crew!");
+  }
+}
+// unreachable statement errror
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+  
+int K=18,U=18;
+while(K<U){
+System.out.print("CodeUniverse");
+}
+System.out.print("Critical Crew!");
+  }
+}
+//op Critical Crew

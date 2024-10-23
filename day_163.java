dd2c0bda248d31d83bed6dbe00151bcdde05d79e
Krishna K <krish1826s@gmail.com>
2024-10-23 13:55:13 +0530

    Create day_163.java

---
+++ day_163.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+     // int a=new Scanner(System.in).nextInt();
+//       if((a&1)==1)
+//       System.out.println("odd");
+//       else{
+//         System.out.println("Even");
+//       }
+      
+//     int count = 0;
+//     for(int j=0;j<=a;j++){
+//       if((j+a)==(j^a)){
+//       // count++;
+//         System.out.println(j);
+//       }
+//     }
+//   // System.out.print(count);
+  
+//   //power of 2
+// int ans=n&(n-1);
+// if(ans==0) power of 2;
+// else not power of 2
+ 
+ 
+// //no of 1 bits
+// int a=7;
+// int oneCount=0;
+// while(a>0){
+// if(a%2==1) oneCount++;
+// a/=2;
+// }
+// System.out.print(oneCount);
+// int[] arr={1,2,3,4,15};
+// System.out.println(sum(arr,arr.length));
+// }
+//   public static int sum(int[] arr,int n){
+//     if(n==1) return arr[0];
+//     return arr[n-1]+sum(arr,n-1);
+//   }
+// }
+
+}
+}

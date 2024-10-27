a7e19440be0d8e5a695fac7058746a3ba5eafb00
Krishna K <krish1826s@gmail.com>
2024-10-27 16:07:08 +0530

    Update day_164.java

--- day_164.java
+++ day_164.java
@@ -16,3 +16,38 @@ public class main{
   }
   
 }
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Matrix Row Blast - Digit 0
+		Scanner sc=new Scanner(System.in);
+		int row=sc.nextInt();
+		int col=sc.nextInt();
+		int[][] mat=new int[row][col];
+		boolean[] increment =new boolean[row];
+		for(int i=0;i<row;i++){
+		    int zeroCount=0;
+		    for(int j=0;j<col;j++){
+		        mat[i][j]=sc.nextInt();
+		        int num=mat[i][j];
+		        int Count = 0;
+		        while(num!=0) {
+		            if(num%10==0) Count=1;
+		            num/=10;
+		        }
+		        zeroCount+=Count;
+		    }
+		    if(zeroCount==1) increment[i]=true;
+		}
+		for(int i=0;i<row;i++){
+		    for(int j=0;j<col;j++){
+		        int printableValue=mat[i][j];
+		        if(increment[i]&&!(printableValue+"").contains("0")) printableValue+=1;
+		        System.out.print(printableValue+" ");
+		    }
+		    System.out.println();
+		}
+
+	}
+}
